269a02bd686912d962bda88a4786dc5355b24ee1
Aleksei <avoroncovs@gmail.com>
2022-02-18 17:12:11 +0200

    backdrop fix in rename channel popup

--- src/RenameChannelModal.jsx
+++ src/RenameChannelModal.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Formik } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
-import { toggleRenameChannelModal } from './slices/chatSlice.js';
+import { toggleNewChannelModal, toggleRenameChannelModal } from './slices/chatSlice.js';
 import SocketContext from './SocketContext.jsx';
 
 export default () => {
@@ -15,63 +15,61 @@ export default () => {
 
   return (
     <>
-      <div className="fade modal-backdrop show"/>
-      <div role="dialog" aria-modal="true" className="fade modal show" tabIndex="-1" style={{ display: 'block' }}>
-        <div className=" modal-dialog modal-dialog-centered">
-          <div className=" modal-content">
-            <div className=" modal-header">
-              <div className=" modal-title h4">Переименовать канал</div>
-              <button aria-label=" Close" data-bs-dismiss=" modal" type="button" className="btn btn-close"/>
-            </div>
-            <div className=" modal-body">
-              <Formik
-                initialValues={{ channelName: currentChannel.name }}
-                validate={(values) => {
-                  if (!values.channelName) {
-                    return { channelName: 'must not be blank' };
-                  }
-                  return {};
-                }}
-                onSubmit={(values, { setSubmitting, setErrors }) => {
-                  setSubmitting(true);
-                  if (channels.find((channel) => channel.name === values.channelName)) {
-                    setErrors({ channelName: 'duplicate channel name' });
-                    setSubmitting(false);
-                  } else {
-                    socket.emit('renameChannel', { name: values.channelName, id: renameChannelModal.id }, (result) => {
-                      // todo chow error message
-                    });
-                  }
-                }}
-              >
-                {
-                  ({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-                    <form onSubmit={handleSubmit}>
-                      <div className=" form-group">
-                        <label className=" visually-hidden" htmlFor=" name">
-                          Имя канала
-                          <input
-                            name="channelName"
-                            id="channelName"
-                            className={`mb-2 form-control ${errors.channelName && touched.channelName ? 'is-invalid' : ''}`}
-                            value={values.channelName}
-                            onChange={handleChange}
-                            autoFocus
-                          />
-                          <div className="invalid-feedback">
-                            {errors.channelName}
-                          </div>
-                        </label>
-                        <div className="d-flex justify-content-end">
-                          <button type="button" className="me-2 btn btn-secondary" onClick={hideModal}>Отменить</button>
-                          <button type="submit" className="btn btn-primary">Отправить</button>
-                        </div>
-                      </div>
-                    </form>
-                  )
+      <div className="fade modal-backdrop show" style={{ zIndex: 1 }} onClick={hideModal}/>
+      <div className=" modal-dialog modal-dialog-centered" style={{ zIndex: 2 }}>
+        <div className=" modal-content">
+          <div className=" modal-header">
+            <div className=" modal-title h4">Переименовать канал</div>
+            <button aria-label=" Close" data-bs-dismiss=" modal" type="button" className="btn btn-close"/>
+          </div>
+          <div className=" modal-body">
+            <Formik
+              initialValues={{ channelName: currentChannel.name }}
+              validate={(values) => {
+                if (!values.channelName) {
+                  return { channelName: 'must not be blank' };
                 }
-              </Formik>
-            </div>
+                return {};
+              }}
+              onSubmit={(values, { setSubmitting, setErrors }) => {
+                setSubmitting(true);
+                if (channels.find((channel) => channel.name === values.channelName)) {
+                  setErrors({ channelName: 'duplicate channel name' });
+                  setSubmitting(false);
+                } else {
+                  socket.emit('renameChannel', { name: values.channelName, id: renameChannelModal.id }, (result) => {
+                    // todo chow error message
+                  });
+                }
+              }}
+            >
+              {
+                ({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+                  <form onSubmit={handleSubmit}>
+                    <div className=" form-group">
+                      <label className=" visually-hidden" htmlFor=" name">
+                        Имя канала
+                        <input
+                          name="channelName"
+                          id="channelName"
+                          className={`mb-2 form-control ${errors.channelName && touched.channelName ? 'is-invalid' : ''}`}
+                          value={values.channelName}
+                          onChange={handleChange}
+                          autoFocus
+                        />
+                        <div className="invalid-feedback">
+                          {errors.channelName}
+                        </div>
+                      </label>
+                      <div className="d-flex justify-content-end">
+                        <button type="button" className="me-2 btn btn-secondary" onClick={hideModal}>Отменить</button>
+                        <button type="submit" className="btn btn-primary">Отправить</button>
+                      </div>
+                    </div>
+                  </form>
+                )
+              }
+            </Formik>
           </div>
         </div>
       </div>
