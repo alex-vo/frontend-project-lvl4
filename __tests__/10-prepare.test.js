6288219bf7ba621b26a982244cbd1ab177ef48fa
Aleksei <avoroncovs@gmail.com>
2022-02-20 17:56:33 +0200

    green test 1

--- __tests__/10-prepare.test.js
+++ __tests__/10-prepare.test.js
@@ -1,8 +1,18 @@
+import 'regenerator-runtime/runtime';
 import path from 'path';
-import { getEntryPointPath } from './helpers/utils.js';
+import readPackageJson from 'read-package-json-fast';
+import _ from 'lodash';
+
+const getEntryPointPath = async (projectPath) => {
+  const packageJsonPath = path.join(projectPath, 'package.json');
+  const packageSource = await readPackageJson(packageJsonPath);
+  // TODO: find a ready-made solution with normalization
+  const main = _.get(packageSource, 'main', 'index.js');
+  return path.join(projectPath, main);
+};
 
 test('entry point', async () => {
-  const codePath = path.join(__dirname, '..', 'code');
+  const codePath = path.join(path.resolve());
   const entryPointPath = await getEntryPointPath(codePath);
 
   const enryPointModule = await import(entryPointPath);
